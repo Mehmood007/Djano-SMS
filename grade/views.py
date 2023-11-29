405e3e263ef7d79890ffc174b21d7479e8ed886a
unknown <mehmoodhassan595@yahoo.com>
2023-11-30 04:05:39 +0500

    Views initialized for project

--- grade/views.py
+++ grade/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+from django.views import View
 
-# Create your views here.
+
+class GradeView(View):
+    """
+    All request regarding student model are handled in this class
+    Function names are based on method names
+    """
+
+    # Below function is triggered upon get request
+    def get(self, request: HttpRequest, id: int) -> HttpResponse:
+        print(type(request))
+        return HttpResponse(f"Hello Grade with id {id}")
