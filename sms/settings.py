6e2a233901710f97a0193a98cfe04c13c014cf72
unknown <mehmoodhassan595@yahoo.com>
2023-12-08 18:10:09 +0500

    Teacher template completed

--- sms/settings.py
+++ sms/settings.py
@@ -59,7 +59,11 @@ ROOT_URLCONF = "sms.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "student\/" / "templates", BASE_DIR / "templates"],
+        "DIRS": [
+            BASE_DIR / "templates",
+            BASE_DIR / "student\/" / "templates",
+            BASE_DIR / "teacher\/" / "templates",
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
